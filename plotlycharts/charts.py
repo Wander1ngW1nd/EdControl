19915b407735581e3141c64df5f51b3a6e0bd797
Misha <krz.m@ya.ru>
2023-09-09 02:00:51 +0300

    worked)  3 min max video
    no progress bar ( no demo inside button (

--- plotlycharts/charts.py
+++ plotlycharts/charts.py
@@ -7,14 +7,16 @@ Created on Wed Sep  6 16:12:00 2023
 
 import plotly.express as px
 import pandas as pd
- 
 
-def radio_chart():
+
+def radio_chart(data):
+    
+    rData=list(data.values())
+    thetaData = list(data.keys())
     
     fig = px.line_polar( 
-                        r = [1, 2, 3, 4, 5, 6],
-                        theta = ["Злость", "Отвращение","Страх",\
-                                 "Счастье","Грусть","Удивление"], 
+                        r = rData,
+                        theta = thetaData, 
                         line_close=True,    
                         color_discrete_sequence=px.colors.sequential.Plasma_r,
                         template="plotly_dark")
@@ -27,13 +29,15 @@ def radio_chart():
 
     return fig
 
-def bar_chart():
+def bar_chart(data):
     
-    df = pd.DataFrame(dict(
-        x = [1, 5, 2, 2, 3, 2],
-        y = ["Злость", "Отвращение","Страх",\
-                 "Счастье","Грусть","Удивление"]))
-    fig = px.bar(df, x = "x", y ='y', barmode = 'group', labels={'x': '', 'y':''}, width=500, height=300)
+    #df = pd.DataFrame(dict(
+     #   x = [1, 5, 2, 2, 3, 2],
+      #  y = ["Злость", "Отвращение","Страх",\
+       #          "Счастье","Грусть","Удивление"]))
+    xData=list(data.values())
+    yData = list(data.keys())
+    fig = px.bar(x = xData, y =yData, barmode = 'group', labels={'x': '', 'y':''}, width=500, height=300)
     #fig.update_layout(showlegend=False)
     fig.update_traces(marker_color = ['#f5800d','#f2ce4d','#047e79','#a69565','#cfc1af','#574c31'], marker_line_color = 'black',
                   marker_line_width = 2, opacity = 1)
