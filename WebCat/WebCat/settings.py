e92e58c922a5072a217e75d5e558a0637a2fada3
Maksim <nicecream66@gmail.com>
2023-09-09 01:33:35 +0300

    commit for deploy

--- WebCat/WebCat/settings.py
+++ WebCat/WebCat/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "46r6m#z&9t$)3#kpz(@)nxxk9(c&wioniddriu*2199#@p7476"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["lalafo.onrender.com", "lalafo.netlify.app", "webcatifmo.herokuapp.com", "127.0.0.1"]
+ALLOWED_HOSTS = ["edcontrol.onrender.com", "127.0.0.1"]
 
 
 # Application definition
@@ -76,27 +76,26 @@ WSGI_APPLICATION = "WebCat.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'd60jdal2ikdanu',
-#         'USER': 'lwjmjvttikslnq',
-#         'PASSWORD': '161396fa891f8db4af26ad91d8a423915cbe01372dd30460b371f60687ee6382',
-#         'HOST': 'ec2-34-197-212-240.compute-1.amazonaws.com',
-#         'PORT': '5432',
-#     }
-# }
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "webcat",
-        "USER": "postgres",
-        "PASSWORD": "root",
-        "HOST": "localhost",
-        "PORT": "5433",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "neondb",
+        "USER": "nicecream66",
+        "PASSWORD": "XA4N3tnizmeB",
+        "HOST": "ep-snowy-truth-23382766.eu-central-1.aws.neon.tech",
+        "PORT": "5432",
     }
 }
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql_psycopg2",
+#         "NAME": "webcat",
+#         "USER": "postgres",
+#         "PASSWORD": "root",
+#         "HOST": "localhost",
+#         "PORT": "5433",
+#     }
+# }
 
 
 # Password validation
