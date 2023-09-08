aad94ab1c36c06dc7e23ab48a6a3dbeb0ef19cee
danil <danislamoff@gmail.com>
2023-09-08 21:39:29 +0300

    implement emotions_timestamps method

--- emrec_model.py
+++ emrec_model.py
@@ -10,6 +10,8 @@ import polars as pl
 from attrs import define, field
 from deepface import DeepFace
 
+_SIGNIFICANT_PERIOD_LENGTH_IN_SECONDS: float = 5
+
 
 class VideoInputException(IOError):
     pass
@@ -34,18 +36,12 @@ class VideoEmotionRecognizer:
             frame: np.ndarray
             return_flag, frame = cap.read()
             if return_flag:
-                result = DeepFace.analyze(
-                    frame, actions="emotion", enforce_detection=False, silent=True
-                )[0]
-                analyzed_frames_data["timestamp"] += [
-                    cap.get(cv2.CAP_PROP_POS_MSEC) / 1000
-                ] * len(result["emotion"].keys())
-                analyzed_frames_data["emotion"] += list(
-                    map(str, result["emotion"].keys())
+                result = DeepFace.analyze(frame, actions="emotion", enforce_detection=False, silent=True)[0]
+                analyzed_frames_data["timestamp"] += [cap.get(cv2.CAP_PROP_POS_MSEC) / 1000] * len(
+                    result["emotion"].keys()
                 )
-                analyzed_frames_data["probability"] += list(
-                    map(float, result["emotion"].values())
-                )
+                analyzed_frames_data["emotion"] += list(map(str, result["emotion"].keys()))
+                analyzed_frames_data["probability"] += list(map(float, result["emotion"].values()))
             else:
                 raise VideoInputException("No video frame returned")
 
@@ -66,6 +62,46 @@ class VideoEmotionRecognizer:
             zip(
                 emotions_summary["emotion"].to_list(),
                 emotions_summary["probability"].to_list(),
+            )
+        )
+
+        return output
+
+    def emotions_timestamps(self) -> dict:
+        emotions_timestamps: pl.DataFrame = (
+            self._analyzed_frames.sort("probability", descending=True)
+            .groupby("timestamp")
+            .first()
+            .sort(by="timestamp", descending=False)
+        )
+
+        emotions_timestamps = emotions_timestamps.with_columns(
+            (pl.col("emotion") != pl.col("emotion").shift_and_fill(pl.col("emotion").backward_fill(), periods=1))
+            .cumsum()
+            .alias("emotion_group")
+        )
+        emotions_timestamps = (
+            emotions_timestamps.groupby(["emotion", "emotion_group"])
+            .agg(
+                pl.col("timestamp").min().alias("emotion_start_timestamp"),
+                pl.col("timestamp").max().alias("emotion_finish_timestamp"),
+            )
+            .drop("emotion_group")
+            .sort(by="emotion_start_timestamp", descending=False)
+        )
+
+        emotions_timestamps = (
+            emotions_timestamps.with_columns(
+                (pl.col("emotion_finish_timestamp") - pl.col("emotion_start_timestamp")).alias("duration")
+            )
+            .filter(pl.col("emotion") != "neutral")
+            .filter(pl.col("duration") > _SIGNIFICANT_PERIOD_LENGTH_IN_SECONDS)
+        )
+
+        output: dict = dict(
+            zip(
+                emotions_timestamps["emotion"].to_list(),
+                emotions_timestamps["emotion_start_timestamp"].to_list(),
             )
         )
 
