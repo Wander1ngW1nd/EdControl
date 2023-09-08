0a32ef2ad8739b77d3326347d425f2b3133efba3
danil <danislamoff@gmail.com>
2023-09-08 19:44:41 +0300

    VideoRemotionRecognizer class with _analyze and emotions_summary methods implemented

--- emrec_model.py
+++ emrec_model.py
@@ -1,49 +1,72 @@
-from paz.pipelines import MiniXceptionFER
-import cv2
-import numpy as np
 import os
 
-path_to_video = os.path.join(
-    '/home/danil/Downloads', 
-    'tomp3.cc - Zoom Class Meeting Downing Sociology 220 10222020_1080p (online-video-cutter.com).mp4'
-)
+os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
-cap = cv2.VideoCapture(path_to_video)
- 
-# Check if video opened successfully
-if (cap.isOpened() == False): 
-  print("Error opening video stream or file")
 
-# outputs probabilities for {“angry”, “disgust”, “fear”, “happy”, “sad”, “surprise”, “neutral”}
-# preserving order
-classifier = MiniXceptionFER()
+import cv2
+import numpy as np
+import polars as pl
+from attrs import define, field
+from deepface import DeepFace
 
-recognised_frames = []
-# Read until video is completed
-while(cap.isOpened()):
-  
-  # ret -- flag, indicating, whether the file returned frame
-  # frame -- video frame (image) itself
-  ret: bool
-  frame: np.array
-  ret, frame = cap.read()
-  if ret == True:
 
-    cv2.imshow('Frame', frame)
+class VideoInputException(IOError):
+    pass
 
-    inference = classifier(frame)
-    inference['timestamp'] = cap.get(cv2.CAP_PROP_POS_MSEC) / 1000
-    print(inference)
- 
-    # Press q on keyboard to exit
-    if (cv2.waitKey(25) & 0xFF) == ord('q'):
-      break
 
-  else: 
-    break
- 
-# When everything done, release the video capture object
-cap.release()
- 
-# Closes all the frames
-cv2.destroyAllWindows()
+@define(slots=True, auto_attribs=True)
+class VideoEmotionRecognizer:
+    filepath: str
+    _analyzed_frames: pl.DataFrame = field(init=False)
+
+    def __attrs_post_init__(self):
+        self._analyzed_frames = self._analyze()
+
+    def _analyze(self) -> pl.DataFrame:
+        cap: cv2.VideoCapture = cv2.VideoCapture(self.filepath)
+        if cap.isOpened() == False:
+            raise VideoInputException("Video opening error")
+
+        analyzed_frames_data: dict = {"timestamp": [], "emotion": [], "probability": []}
+        while cap.isOpened():
+            return_flag: bool
+            frame: np.ndarray
+            return_flag, frame = cap.read()
+            if return_flag:
+                result = DeepFace.analyze(
+                    frame, actions="emotion", enforce_detection=False, silent=True
+                )[0]
+                analyzed_frames_data["timestamp"] += [
+                    cap.get(cv2.CAP_PROP_POS_MSEC) / 1000
+                ] * len(result["emotion"].keys())
+                analyzed_frames_data["emotion"] += list(
+                    map(str, result["emotion"].keys())
+                )
+                analyzed_frames_data["probability"] += list(
+                    map(float, result["emotion"].values())
+                )
+            else:
+                raise VideoInputException("No video frame returned")
+
+        return pl.DataFrame(analyzed_frames_data)
+
+    def emotions_summary(self) -> dict:
+        emotions_summary: pl.DataFrame = (
+            self._analyzed_frames.groupby("emotion")
+            .agg(pl.col("probability").sum())
+            .sort("probability", descending=True)
+        )
+
+        emotions_summary = emotions_summary.with_columns(
+            (pl.col("probability") / pl.sum("probability")).alias("probability")
+        )
+
+        output: dict = dict(
+            zip(
+                emotions_summary["emotion"].to_list(),
+                emotions_summary["probability"].to_list(),
+            )
+        )
+
+        return output
